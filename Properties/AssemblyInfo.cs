02318cf66ceb96bbbe9c98b7cc48a13f6eb9898d
rube200 <rube.g.rube@gmail.com>
2017-06-28 10:51:16 +0100

    Libraries

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PlayerReport")]
+[assembly: AssemblyProduct("PlayerReport")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("10a1820f-f969-4505-9e26-7bb0726e27e1")]
+[assembly: AssemblyVersion("1.0.0.0")]
